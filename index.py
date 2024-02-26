14ed16190cb9a0a2f0fe75fec9b36d62a2d87d16
Pedro Henrique Mazocco <160538422+PedroMazocco@users.noreply.github.com>
2024-02-25 21:57:14 -0300

    commit

--- index.py
+++ index.py
@@ -1,11 +1,12 @@
 from PyPDF2 import PdfMerger
+import os
 
-pdf_list = ['pdf1.pdf', 'pdf2.pdf']
+pdf_list = os.listdir("pdfsJuntar")
 
 juntar = PdfMerger()
 
 for pdf in pdf_list:
-    juntar.append(pdf)
+    juntar.append(f'pdfsJuntar/{pdf}')
 
 juntar.write('pronto.pdf')
 print("PDF pronto!")
